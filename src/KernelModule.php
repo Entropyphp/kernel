4479cf852524b8a95d9e30ffa2246f530ada9c16
willy <william.lety@gmail.com>
2025-08-24 18:10:08 +0200

    First commit

---
+++ src/KernelModule.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entropy;
+
+use Entropy\Module;
+
+//@codeCoverageIgnoreStart
+class KernelModule extends Module
+{
+    public const DEFINITIONS = __DIR__ . '/config.php';
+}
+//@codeCoverageIgnoreEnd
